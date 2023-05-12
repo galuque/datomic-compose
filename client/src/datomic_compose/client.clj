24c36af9c91eb4b3962c2f690b35baacf77fc262
galuque <gldidonna@gmail.com>
2023-05-12 18:49:12 -0300

    fix: add license

--- client/src/datomic_compose/client.clj
+++ client/src/datomic_compose/client.clj
@@ -1,3 +1,12 @@
+;   Example adapted from: https://github.com/cognitect-labs/day-of-datomic-cloud
+;   Copyright (c) Cognitect, Inc. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
 (ns datomic-compose.client
   (:require [datomic.client.api :as d]))
 
@@ -11,12 +20,13 @@
 
 (def conn (d/connect client {:db-name "datomic"}))
 
-;; Get the current value of the database:
 (def db (d/db conn))
 
-;; Create a query for all movie titles:
 (def all-titles-q '[:find ?movie-title
                     :where [_ :movie/title ?movie-title]])
 
-;; Execute the query with the value of the database:
-(d/q all-titles-q db)
+(comment
+  
+  (d/q all-titles-q db)
+  
+  )
--- peer/src/datomic_compose/peer.clj
+++ peer/src/datomic_compose/peer.clj
@@ -1,3 +1,12 @@
+;   Example adapted from: https://github.com/cognitect-labs/day-of-datomic-cloud
+;   Copyright (c) Cognitect, Inc. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
 (ns datomic-compose.peer
   (:require [datomic.api :as d]))
 
@@ -20,8 +29,10 @@
                     :db/cardinality :db.cardinality/one
                     :db/doc "The year the movie was released in theaters"}])
 
-;; Now transact the schema:
-(d/transact conn movie-schema)
+(comment
+   ;; Now transact the schema:
+  (d/transact conn movie-schema)
+  )
 
 ;; Define some movies to add to the database:
 (def first-movies [{:movie/title "The Goonies"
@@ -34,15 +45,21 @@
                     :movie/genre "punk dystopia"
                     :movie/release-year 1984}])
 
-;; Now transact the movies:
-(d/transact conn first-movies)
+(comment
+   ;; Now transact the movies:
+  (d/transact conn first-movies)
+  )
+  
 
 ;; Get the current value of the database:
 (def db (d/db conn))
+  
 
 ;; Create a query for all movie titles:
 (def all-titles-q '[:find ?movie-title
                     :where [_ :movie/title ?movie-title]])
 
-;; Execute the query with the value of the database:
-(d/q all-titles-q db)
+(comment
+  ;; Execute the query with the value of the database:
+  (d/q all-titles-q db)
+  )
