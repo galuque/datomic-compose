9ddea7267f8ab9e2ff8c21f7f3c7bfb8bbab4499
galuque <gldidonna@gmail.com>
2023-05-12 18:48:31 -0300

    fix: remove sleep

--- files/peer-server.sh
+++ files/peer-server.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-sleep 15
-
 /opt/datomic/bin/run -m datomic.peer-server -h 0.0.0.0 -p 8998 -Ddatomic.memcachedServers=memcached:11211 -a accesskey,secret -d 'datomic,datomic:sql://datomic?jdbc:postgresql://postgres:5432/datomic?user=datomic&password=datomic'
