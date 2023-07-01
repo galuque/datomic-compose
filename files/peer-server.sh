c950dbd942522e269e202b01cc339653fed6ce33
galuque <gldidonna@gmail.com>
2023-07-01 20:42:32 -0300

    fix: format script

--- files/peer-server.sh
+++ files/peer-server.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-/opt/datomic/bin/run -m datomic.peer-server -h 0.0.0.0 -p 8998 -Ddatomic.memcachedServers=memcached:11211 -a accesskey,secret -d 'datomic,datomic:sql://datomic?jdbc:postgresql://postgres:5432/datomic?user=datomic&password=datomic'
+/opt/datomic/bin/run -m datomic.peer-server \
+                     -h 0.0.0.0 \
+                     -p 8998 \
+                     -Ddatomic.memcachedServers=memcached:11211 \
+                     -a accesskey,secret \
+                     -d 'datomic,datomic:sql://datomic?jdbc:postgresql://postgres:5432/datomic?user=datomic&password=datomic'
